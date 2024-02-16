acdd2520408c0e47f3b88d837748d8169ceb0adf
Ilham Fathullah <ilhamfathullah04@gmail.com>
2024-02-16 17:16:03 +0700

    Day 5 - Count Post Blog Time

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,15 +5,15 @@ function addBlog(event) {
 
   let title = document.getElementById("input-blog-title").value;
   let content = document.getElementById("input-blog-content").value;
+  let postAt = new Date();
 
   let dataBlog = {
     title,
     content: content,
+    postAt,
   };
 
   dataBlogs.push(dataBlog);
-
-  console.log(dataBlogs);
 
   renderBlog();
 }
@@ -38,13 +38,75 @@ function renderBlog() {
         >
       </h1>
       <div class="detail-blog-content">
-        12 Jul 2021 22:30 WIB | Ichsan Emrald Alamsyah
+      ${getFullDate(dataBlogs[index].postAt)} | Ichsan Emrald Alamsyah
       </div>
       <p>
       ${dataBlogs[index].content}
       </p>
+
+      <div style="float:right">
+          <p style="color:grey;">${getDistanceTime(dataBlogs[index].postAt)}</p>
+      </div>
     </div>
   </div>
     `;
+  }
+}
+
+function getFullDate(time) {
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "Mei",
+    "Jun",
+    "Jul",
+    "Ags",
+    "Sep",
+    "Okt",
+    "Nov",
+    "Des",
+  ];
+
+  const year = time.getFullYear();
+  const month = time.getMonth();
+  const date = time.getDate();
+
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+
+  return `${date} ${monthNames[month]} ${year} - ${hour}:${minute} WIB`;
+}
+
+function getDistanceTime(time) {
+  // 1. kapan diposting => time
+  // 2. waktu sekarang =>  new Date();
+  // result = waktu sekarang - kapan diposting
+
+  let timeNow = new Date();
+  let timePost = time;
+  let distanceTime = timeNow - timePost; // dalam bentuk milisecond
+  console.log(distanceTime);
+
+  let milisecond = 1000; //milisecond
+  let secondInHour = 3600; // 1 jam = 3600 detik
+  let hourInDay = 24; // 1 hari = 24 jam
+
+  let distanceDay = Math.floor(
+    distanceTime / (milisecond * secondInHour * hourInDay)
+  );
+  let distanceHour = Math.floor(distanceTime / (milisecond * secondInHour));
+  let distanceMinute = Math.floor(distanceTime / (milisecond * 60));
+  let distanceSecond = Math.floor(distanceTime / milisecond);
+
+  if (distanceDay > 0) {
+    return `${distanceDay} days ago`;
+  } else if (distanceHour > 0) {
+    return `${distanceHour} hours ago`;
+  } else if (distanceMinute > 0) {
+    return `${distanceMinute} minutes ago`;
+  } else {
+    return `${distanceSecond} seconds ago`;
   }
 }
